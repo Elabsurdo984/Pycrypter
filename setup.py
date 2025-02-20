922130c47c86d0293caf01748db53961b033993a
Elabsurdo984 <matiassfernandez00@gmail.com>
2025-02-20 18:53:06 -0300

    1.0.0

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pycrypter",
+    name="cryptpy",
     version="1.0.0",
     packages=find_packages(),
     install_requires = [
-        "pycryptodome>=3.21.0"
+        "pycryptodome>=3.21.0",
         "numpy>=2.0.2"
     ],
     author="Elabsurdo984",
